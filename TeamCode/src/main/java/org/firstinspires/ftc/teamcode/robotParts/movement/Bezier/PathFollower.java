4d44004ebb69bff7d7842b3420f248b929bd8284
GerstenJoch <sanderr06@gmail.com>
2024-06-08 10:11:13 +0200

    Path following bijna klaar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/movement/Bezier/PathFollower.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotParts/movement/Bezier/PathFollower.java
@@ -13,34 +13,43 @@ public class PathFollower {
     double Fcent, Ftrans, Fcorr;
     double Fcent_weight, Ftrans_weigth;
     double v_robot = 5; //TODO: Omzetten naar realtime snelheid
+    double robotHeading = 45; //TODO: Omzetten naar realtime heading
     public void followPath(double[][] controlPoints) {
         path.buildPath(controlPoints);
-        for (int i = 0; i < coordinate.length; i++) {
-            coordinate = path.coordinate[i];
-            trans_dist = Math.pow(coordinate[0]-robot_position[0],2) + Math.pow(coordinate[1] - robot_position[1],2);
-            if (i != 0) {
-                if ((shortest_dist == 0) || (Math.abs(trans_dist) < shortest_dist) && !Double.isNaN(trans_dist)) {
+        double coordinateLength = path.coordinate.length;
+        while (shortest_time < 100) {
+            for (int i = shortest_time; i < coordinateLength; i++) {
+                coordinate = path.coordinate[i];
+                trans_dist = Math.pow(coordinate[0] - robot_position[0], 2) + Math.pow(coordinate[1] - robot_position[1], 2);
+                if (i != 0) {
+                    if ((shortest_dist == 0) || (Math.abs(trans_dist) < shortest_dist) && !Double.isNaN(trans_dist)) {
+                        shortest_dist = Math.abs(trans_dist);
+                        shortest_time = i;
+                    }
+                } else {
                     shortest_dist = Math.abs(trans_dist);
                     shortest_time = i;
                 }
-            } else {
-                shortest_dist = Math.abs(trans_dist);
             }
-        }
-        coordinate = path.coordinate[shortest_time];
-        sec_derivative = path.sec_derivative[shortest_time];
-        derivative = path.derivative[shortest_time];
-        r_circle = path.r_circle[shortest_time];
-        d2 = (sec_derivative[1]*derivative[0] - sec_derivative[0]*derivative[1])/Math.pow(sec_derivative[0],2);
-        relative_pos = (coordinate[0] - robot_position[0]) + (coordinate[1] - robot_position[1]);
-        Fcent = Fcent_weight * Math.pow(v_robot,2) / r_circle;
-        if (d2 < 0) Fcent *= -1;
-        Ftrans = -Math.sqrt(shortest_dist);
-        if ((d2 < 0 && relative_pos < 0) || (d2 > 0 && relative_pos > 0)) Ftrans *= -1;
-        Fcorr = Ftrans + Fcent;
-        if (Fcorr > 1 || Fcorr < -1) {
-            if (Fcorr > 1) Fcorr = 1;
-            else Fcorr = -1;
+            coordinate = path.coordinate[shortest_time];
+            sec_derivative = path.sec_derivative[shortest_time];
+            derivative = path.derivative[shortest_time];
+            r_circle = path.r_circle[shortest_time];
+            d2 = (sec_derivative[1] * derivative[0] - sec_derivative[0] * derivative[1]) / Math.pow(sec_derivative[0], 2);
+            relative_pos = (coordinate[0] - robot_position[0]) + (coordinate[1] - robot_position[1]);
+            Fcent = Fcent_weight * Math.pow(v_robot, 2) / r_circle;
+            if (d2 < 0) Fcent *= -1;
+            Ftrans = -Math.sqrt(shortest_dist);
+            if ((d2 < 0 && relative_pos < 0) || (d2 > 0 && relative_pos > 0)) Ftrans *= -1;
+            Fcorr = Ftrans + Fcent;
+            if (Fcorr > 1 || Fcorr < -1) {
+                if (Fcorr > 1) Fcorr = 1;
+                else Fcorr = -1;
+            }
+            double theta1 = Math.asin(Fcorr/*/motor.speed*/);
+            double theta2 = Math.toRadians(robotHeading);
+            double theta4 = Math.atan(derivative[1] / derivative[0]);
+            double driveDirection = 90 - theta1 - theta2 + theta4;
         }
     }
 }
